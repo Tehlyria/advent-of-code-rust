84ff5af98f517a47da9180247e2871b713a81880
DanielBelow <daniel.below@tum.de>
2022-12-31 01:43:01 +0100

    Remove unnecessary dependency

--- src/day07.rs
+++ src/day07.rs
@@ -1,5 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use permutohedron::LexicalPermutation;
+use itertools::{izip, Itertools};
 
 use crate::intcode::{IntCode, State};
 
@@ -8,25 +8,16 @@ pub fn generate(inp: &str) -> Vec<i64> {
     inp.split(',').filter_map(|it| it.parse().ok()).collect()
 }
 
-fn permutate(mut data: [i64; 5]) -> Vec<Vec<i64>> {
-    let mut permutations = vec![data.to_vec()];
-
-    while data.next_permutation() {
-        permutations.push(data.to_vec());
-    }
-
-    permutations
-}
-
 #[aoc(day7, part1)]
 pub fn part1(mem: &[i64]) -> Option<i64> {
-    permutate([0, 1, 2, 3, 4])
+    [0, 1, 2, 3, 4]
         .iter()
+        .permutations(5)
         .map(|it| {
-            it.iter().fold(0, |acc, elem| {
+            it.iter().fold(0, |acc, &&elem| {
                 let mut vm = IntCode::new(mem);
 
-                match vm.run_with_input(0, &[*elem, acc]) {
+                match vm.run_with_input(0, &[elem, acc]) {
                     State::Write(n) | State::Halted(n) => n,
                     State::Waiting => panic!("Too many inputs?"),
                 }
@@ -37,8 +28,9 @@ pub fn part1(mem: &[i64]) -> Option<i64> {
 
 #[aoc(day7, part2)]
 pub fn part2(mem: &[i64]) -> Option<i64> {
-    permutate([5, 6, 7, 8, 9])
+    [5, 6, 7, 8, 9]
         .iter()
+        .permutations(5)
         .map(|it| {
             let mut last = 0;
             let mut last_output = 0;
@@ -55,11 +47,8 @@ pub fn part2(mem: &[i64]) -> Option<i64> {
             loop {
                 let inp_idx = usize::from(!needs_phases);
 
-                for i in 0..it.len() {
-                    let it_val = it[i];
-                    let cur_vm = &mut vms[i];
-
-                    match cur_vm.run_with_input(inp_idx, &[it_val, last]) {
+                for (&&val, vm) in izip!(&it, &mut vms) {
+                    match vm.run_with_input(inp_idx, &[val, last]) {
                         State::Halted(_) => return last_output,
                         State::Write(n) => {
                             last_output = n;
